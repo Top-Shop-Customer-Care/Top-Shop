ce467c013d21b11cd6b9f39b4487b3c7f29717cc
Top-Shop-Customer-Care <113470100+Top-Shop-Customer-Care@users.noreply.github.com>
2022-09-14 00:29:35 +0530

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,27 @@
+function antique(){
+    window.location.replace("antique.html");
+}
+
+function le(){
+    window.location.replace("le.html");
+}
+
+function ri(){
+    window.location.replace("ri.html");
+}
+
+function ge(){
+    window.location.replace("ge.html");
+}
+
+function si(){
+    window.location.replace("si.html");
+}
+
+function gaming(){
+    window.location.replace("gaming.html");
+}
+
+function child(){
+    window.location.replace("child.html");
+}
---
+++ home.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location.replace("home.html");
+}
+
+function categories(){
+    window.location.replace("categories.html");
+}
+
+function contact(){
+    window.location.replace("contact.html");
+}
+
+function about(){
+    window.location.replace("about.html");
+}
